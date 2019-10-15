5805b3c3a8ca90d466526679938e16d5392548ec
zhangmingwei <zjjzmw1@163.com>
2019-10-15 11:49:26 +0800

    UIAlertController+XMSTool 黑暗模式适配

--- XMS_UIKit/XMS_UIAlertController/UIAlertController+XMSTool.swift
+++ XMS_UIKit/XMS_UIAlertController/UIAlertController+XMSTool.swift
@@ -19,6 +19,7 @@ import Foundation
     @objc  public func addMyAction(title: String?, style: UIAlertAction.Style, handler: ((UIAlertAction) -> Swift.Void)? = nil) {
         let action = UIAlertAction(title: title, style: style, handler: handler)
         self.addAction(action)
+        
     }
     /// 弹出AlertV - 第三步
     @objc  public func showAlertC(vc: UIViewController!, completion: (() -> Void)?) {
@@ -29,6 +30,7 @@ import Foundation
         } else {
             vc.present(self, animated: true, completion: completion)
         }
+        self.changeBgColor(bgColor: UIColor.white)
     }
     /// 弹出AlertV - 第三步  actionSheet并且需要适配pad的情况.     sourceFrame : pad的时候，弹出的初始位置(pad强迫必须有，否则崩溃)
     @objc  public func showAlertCWithSourceView(sourceFrame:CGRect, vc: UIViewController!, completion: (() -> Void)?) {
@@ -39,10 +41,31 @@ import Foundation
         } else {
             vc.present(self, animated: true, completion: completion)
         }
+        self.changeBgColor(bgColor: UIColor.white)
     }
     
-    /// 自定义控件的背景色   ---- iOS13黑暗模式的时候，可能会用到
-    @objc public func changeBgColor(bgColor: UIColor) {
+    /// 自定义控件的背景色   ---- iOS13黑暗模式的时候，可能会用到  bgIsLight : 是否背景是浅色的，默认是浅色的
+    @objc public func changeBgColor(bgColor: UIColor, bgIsLight:Bool = true) {
+        var titleColor = UIColor.white  // title颜色
+        var msgColor   = UIColor.white  // message颜色
+        
+        if bgIsLight == true { // 修改标题颜色
+            titleColor = UIColor.black
+            msgColor = UIColor.black
+        } else {
+            titleColor = UIColor.white
+            msgColor = UIColor.white
+        }
+        
+        // 标题颜色
+        let titleStr: NSMutableAttributedString = NSMutableAttributedString.init(string: self.title ?? "")
+        titleStr.addAttributes([.foregroundColor:titleColor], range: NSMakeRange(0,self.title?.length ?? 0))
+        self.setValue(titleStr, forKey: "attributedTitle")
+        let msgStr: NSMutableAttributedString = NSMutableAttributedString.init(string: self.message ?? "")
+        msgStr.addAttributes([.foregroundColor:msgColor], range: NSMakeRange(0,self.message?.length ?? 0))
+        self.setValue(msgStr, forKey: "attributedMessage")
+        
+        // 按钮的背景色
         let FirstSubview = self.view.subviews.first
         let AlertContentView = FirstSubview?.subviews.first
         for subview in (AlertContentView?.subviews)! {
